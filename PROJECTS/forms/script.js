fae5b4d697bf7066f6a2586adefe94d3baa5e531
Abel-Ronoh <abellronoh@gmail.com>
2024-05-23 21:58:15 +0300

    first project

---
+++ PROJECTS/forms/script.js
@@ -0,0 +1,23 @@
+document.getElementById("signUpForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    var username = document.getElementById("username").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+    var message = document.getElementById("message")
+
+    console.log(password.length)
+    if(password.length < 8){
+        message.textContent = "Password is too short"
+        return;
+    }
+    if (username == "" || password == "" || email == ""){
+        message.textContent = "All input fields need to be filled"
+        return;
+    }
+    else{
+        message.style.color = 'green'
+        message.textContent = "you have signed up successfuly"
+        return;
+    }
+
+})
